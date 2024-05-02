1f10b1a3cc71bd4ddebe01239f781198b9421745
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-05-01 22:25:05 -0300

    HW D3 v2

--- back/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var usersController_1 = require("../controllers/usersController");
-var router = (0, express_1.Router)();
-router.get("/users", usersController_1.getUsers);
-router.post("/users", usersController_1.createUser);
-router.delete("/users", usersController_1.deleteUser);
-exports.default = router;
--- back/dist/routes/indexRouter.js
+++ back/dist/routes/indexRouter.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var usersRouter_1 = __importDefault(require("./usersRouter"));
+var appointmentRouter_1 = __importDefault(require("./appointmentRouter"));
 var indexRouter = (0, express_1.Router)();
 indexRouter.use("/users", usersRouter_1.default);
+indexRouter.use("/appointments", appointmentRouter_1.default);
 exports.default = indexRouter;
