4c16068e935a675dc2112e1f1680826dc104fbfa
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-05-10 19:45:03 -0300

    HW D10v2

--- front/vite-project/src/views/MisTurnos/MisTurnos.jsx
+++ front/vite-project/src/views/MisTurnos/MisTurnos.jsx
@@ -14,14 +14,14 @@ const MisTurnos = () => {
 
     return (
     <>
-    <div className={styles.indice}>
-        <h1>Fecha</h1>
-        <h1>Horario</h1>
-        <h1>Cancha</h1>
-        <h1>Estado</h1>
-        <h1>Cancelar</h1>
-    </div>
-        <div>
+        <div className={styles.indice}>
+            <h1>Fecha</h1>
+            <h1>Horario</h1>
+            <h1>Cancha</h1>
+            <h1>Estado</h1>
+            <h1>Cancelar</h1>
+        </div>
+        <div className={styles.container}>
             {turnos.map(turno => {
                 const fecha = turno.date.split('T')[0]
                 return <Turno key={turno.id} id={turno.id} date={fecha} time={turno.time} type={turno.type} status={turno.status} />
